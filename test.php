ea3948df3559ede7e3c4d78c426a5cbab29eff40
devkeep <devkeep@skeep.cc>
2020-05-08 18:14:25 +0800

    update

--- test.php
+++ test.php
@@ -6,10 +6,10 @@ require __DIR__ . '/vendor/autoload.php';
 $res = devkeep\Tools\Tools::format(100);
 
 // 二维数组排序
-// $res = Tools\Tools::arrayMultiSort([
-//     ['id' => 1],
-//     ['id' => 2],
-//     ['id' => 3],
-// ], 'id', 'asc');
+$res = devkeep\Tools\Tools::arrayMultiSort([
+    ['id' => 1],
+    ['id' => 2],
+    ['id' => 3],
+], 'id', 'asc');
 
 var_dump($res);
